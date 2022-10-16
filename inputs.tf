3bcf26cfd0d9151d6d8c51d3cbbef6a5ccc485ff
kishore <kishorebittu3003@gmail.com>
2022-10-16 06:13:23 +0530

    added pipeline syntax

---
+++ inputs.tf
@@ -0,0 +1,10 @@
+variable "target-vpc" {
+    type          = string
+    default       = "ap-south-1"
+}
+
+
+variable "cidr-range"{
+    type          =  string
+    default       ="10.0.0.0/16"
+}
